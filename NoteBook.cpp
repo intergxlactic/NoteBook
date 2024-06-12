df0ed4e89e0cecc09749635499345cd23f92b0c4
immunity.heart123@gmail.com <immunity.heart123@gmail.com>
2024-06-12 19:52:23 +0300

    Добавьте файлы проекта.

---
+++ NoteBook.cpp
@@ -0,0 +1,77 @@
+﻿//Проект - блокнот в котором человек задает команду 
+// 1(посмотреть свой блокнот) и 2(добавить новый пункт в свой блокнот)
+
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void addItemInNoteBook(const string& filename, const vector<string>& lines) {
+	ofstream file(filename, ios::app);
+		if (file.is_open()) {
+			for (const auto& line : lines) {
+				file << line << endl;
+			}
+}
+	file.close();
+}
+
+void viewNoteBook(string filename) {
+	ifstream file(filename);
+
+	string line;
+	int lineNumber = 0;
+
+	if (file.is_open()) {
+		while (getline(file, line)) {
+			lineNumber++;
+			cout << lineNumber << " " << line << endl;
+		}
+	}
+	file.close();
+}
+
+string reading() {
+	string line;
+	cin >> line;
+	return line;
+}
+
+
+void notebook(string file) {
+func:
+	vector<string> line;
+	cout << "Enter the command (1,2): " << endl;
+	cout << "1 - View your notebook" << endl;
+	cout << "2 - Add items to the notebook" << endl;
+	cout << "3 - Delete item in notebook" << endl;
+	cout << "q - Exiting the programme" << endl;
+
+	char input = _getch();
+
+	switch (input) {
+	case '1':
+		viewNoteBook(file);
+		goto func;
+	case '2':
+		cout << "Enter information:" << endl;
+		line = { reading() };
+		addItemInNoteBook(file, line);
+		system("CLS");
+		goto func;
+	case '3':
+		deleteItem(file); // при нажатии на 3 сделать функцию удаления строки из файла
+	case 'q':
+		exit(0);
+	}
+	
+}
+
+int main() {
+
+	string file = "NoteBook.txt";
+
+	notebook(file);
+}
